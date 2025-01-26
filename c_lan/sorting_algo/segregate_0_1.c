7fb2c1412a81a7cf90bb1a10dc1fa5e992818068
Rahul Moorkoth <echobbyist44@gmail.com>
2025-01-26 18:20:07 +0530

    Merge functions in segregate_0_1

--- c_lan/sorting_algo/segregate_0_1.c
+++ c_lan/sorting_algo/segregate_0_1.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
+// {0,1,1,0,1}
 
+// 1st Method
+void sort_array(int *arr, int size) {
+    int *start = arr;
+    int *end = arr + (size - 1);
+    int temp;
+
+    while (start < end) {
+        if (*start == 0) {
+            start++;
+        } else if (*end == 1) {
+            end--;
+        } else {
+            // Swap values
+            temp = *start;
+            *start = *end;
+            *end = temp;
+            start++;
+            end--;
+        }
+    }
+}
+
+// 2nd Method
 void segregate0and1(int arr[], int n) {
     int lo = 0, hi = n - 1;
     while (lo < hi) {
@@ -18,7 +42,8 @@ void segregate0and1(int arr[], int n) {
 int main() {
     int arr[] = {0, 1, 0, 1, 1, 1};
     int n = sizeof(arr) / sizeof(arr[0]);
-    segregate0and1(arr, n);
+    sort_array(arr, n);
+    //segregate0and1(arr, n);
     printf("Array after segregation: ");
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
